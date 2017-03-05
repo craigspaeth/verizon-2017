19766f077fb7e757661906e9a3116722ab4754c6
craigspaeth <craigspaeth@gmail.com>
2017-03-05 15:00:27 -0500

    AR home videos wired up.

--- source/js/index.js
+++ source/js/index.js
@@ -1,8 +1,49 @@
+//
+// Main app javascript that uses jQuery to hook up functionality.
+// Wondering where semicolons are? This is linted with StandardJS.com
+//
 (function () {
   var $ = window.$
 
   // Toggle hamburger menu
   $('.header-content-burger').click(function () {
     $('.header').toggleClass('open')
+  })
+
+  // Open video
+  $('.ar-slide-play-button').click(function () {
+    var $main = $(this).siblings('.ar-slide-video-main')
+    $main.show()
+    $main.find('.ar-slide-video').get(0).play()
+  })
+
+  // Video controls
+  $('.ar-slide-video-controls-pause').click(function () {
+    var $play = $(this).siblings('.ar-slide-video-controls-play')
+    var $video = $(this).parent().siblings('.ar-slide-video')
+    $(this).hide()
+    $video.get(0).pause()
+    $play.show()
+  })
+  $('.ar-slide-video-controls-play').click(function () {
+    var $pause = $(this).siblings('.ar-slide-video-controls-pause')
+    var $video = $(this).parent().siblings('.ar-slide-video')
+    $(this).hide()
+    $video.get(0).play()
+    $pause.show()
+  })
+  $('.ar-slide-video-controls-mute').click(function () {
+    var $sound = $(this).siblings('.ar-slide-video-controls-sound')
+    var $video = $(this).parent().siblings('.ar-slide-video')
+    $(this).hide()
+    $video.prop('muted', false)
+    $sound.show()
+  })
+  $('.ar-slide-video-controls-sound').click(function () {
+    var $mute = $(this).siblings('.ar-slide-video-controls-mute')
+    var $video = $(this).parent().siblings('.ar-slide-video')
+    $(this).hide()
+    $video.prop('muted', true)
+    $mute.show()
   })
 })()
