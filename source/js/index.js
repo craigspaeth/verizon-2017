3b3bc95c2b0d695c180c861413dd8feab294a91d
craigspaeth <craigspaeth@gmail.com>
2017-03-05 19:07:27 -0500

    More or less done with homepage.

--- source/js/index.js
+++ source/js/index.js
@@ -2,17 +2,18 @@
 // Main app javascript that uses jQuery to hook up functionality.
 // Wondering where semicolons are? This is linted with StandardJS.com
 //
-(function () {
-  var $ = window.$
+var $ = window.$
+var _ = window._
 
+$(function () {
   // Toggle hamburger menu
   $('.header-content-burger').click(function () {
     $('.header').toggleClass('open')
   })
 
   // Open video
-  $('.ar-slide-play-button').click(function () {
-    var $main = $(this).siblings('.ar-slide-video-main')
+  $('.ar-slide-play-button, .ar-slide-play-mobile').click(function () {
+    var $main = $(this).closest('.ar-slide').find('.ar-slide-video-main')
     $main.show()
     $main.find('.ar-slide-video').get(0).play()
   })
@@ -23,27 +24,87 @@
     var $video = $(this).parent().siblings('.ar-slide-video')
     $(this).hide()
     $video.get(0).pause()
-    $play.show()
+    $play.css({ display: 'inline-block' })
   })
   $('.ar-slide-video-controls-play').click(function () {
     var $pause = $(this).siblings('.ar-slide-video-controls-pause')
     var $video = $(this).parent().siblings('.ar-slide-video')
     $(this).hide()
     $video.get(0).play()
-    $pause.show()
+    $pause.css({ display: 'inline-block' })
   })
   $('.ar-slide-video-controls-mute').click(function () {
     var $sound = $(this).siblings('.ar-slide-video-controls-sound')
     var $video = $(this).parent().siblings('.ar-slide-video')
     $(this).hide()
     $video.prop('muted', false)
-    $sound.show()
+    $sound.css({ display: 'inline-block' })
   })
   $('.ar-slide-video-controls-sound').click(function () {
     var $mute = $(this).siblings('.ar-slide-video-controls-mute')
     var $video = $(this).parent().siblings('.ar-slide-video')
     $(this).hide()
     $video.prop('muted', true)
-    $mute.show()
+    $mute.css({ display: 'inline-block' })
   })
-})()
+  $('.ar-slide-video-controls-close').click(function () {
+    var $main = $(this).parent('.ar-slide-video-main')
+    var vid = $main.find('.ar-slide-video').get(0)
+    vid.pause()
+    vid.currentTime = 0
+    $main.hide()
+  })
+
+  // Scroll jacking
+  if (window.location.pathname !== '/') return
+  var slideIndex = 0
+  var sliding = false
+
+  var scrollToSlide = function (i) {
+    if (sliding) return
+    sliding = true
+    slideIndex = i
+    $('body, html').animate({
+      scrollTop: ($(window).height() * i) - ($('.header').height() * i)
+    }, 250, 'swing', function () {
+      sliding = false
+    })
+    $('.ar-dot').removeClass('ar-dot-active')
+    $('.ar-dot:nth-child(' + (i + 1) + ')').addClass('ar-dot-active')
+  }
+
+  var slideDir = function (dir) {
+    if (sliding) return
+    if (dir === 'up' && slideIndex !== 0) {
+      scrollToSlide(slideIndex - 1)
+    } else if (dir === 'down' && slideIndex < $('.ar-slide').length) {
+      scrollToSlide(slideIndex + 1)
+    }
+  }
+
+  // Lock the body and scroll to top on load
+  $('body').css({ overflow: 'hidden' })
+  setTimeout(function () { scrollToSlide(0) })
+
+  // On scroll event
+  $(window).on('mousewheel', _.debounce(function (e) {
+    var dir = e.originalEvent.wheelDelta > 0 ? 'up' : 'down'
+    slideDir(dir)
+  }, 50, true))
+
+  // Up and down arrow slide
+  $(document).keydown(_.debounce(function (e) {
+    if (e.which === 38) slideDir('up')
+    else if (e.which === 40) slideDir('down')
+  }, 200))
+
+  // On click dot
+  $('.ar-dot').click(function () {
+    scrollToSlide($(this).index())
+  })
+
+  // On resize window
+  $(window).on('resize', _.debounce(function () {
+    scrollToSlide(slideIndex)
+  }, 300))
+})
